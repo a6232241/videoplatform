c46a5f95aedf38e85b1476d68e46e3f115bfe7ff
a55663340 <a6232241@g.pccu.edu.tw>
2020-08-22 21:23:08 +0800

    修改npm start啟動方式

---
+++ app.js
@@ -0,0 +1,16 @@
+"use strict"
+
+var fs = require('fs')
+var path = require('path')
+var express = require('express')
+const PORT = process.env.PORT || 5000
+
+var app = express()
+app.use(express["static"](path.resolve(__dirname, './dist')))
+app.get('*', function (req, res) {
+  var html = fs.readFileSync(path.resolve(__dirname, './dist/index.html'), 'utf-8')
+  res.send(html)
+})
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
+})
